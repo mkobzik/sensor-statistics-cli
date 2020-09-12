db908ab9893c61e666478105078050c6898ccbea
Michał Kobzik <michal.kobzik@gmail.com>
2020-09-13 01:25:49 +0200

    Add sensor and humidity models

---
+++ src/main/scala/com/github/mkobzik/sensorstatisticscli/models.scala
@@ -0,0 +1,21 @@
+package com.github.mkobzik.sensorstatisticscli
+
+import com.github.mkobzik.sensorstatisticscli.models.Sensor.Id
+import io.estatico.newtype.macros.newtype
+
+object models {
+
+  final case class Sensor(id: Id)
+
+  object Sensor {
+    @newtype final case class Id(value: Int)
+  }
+
+  sealed trait Humidity
+
+  object Humidity {
+    final case class Measured(value: Int) extends Humidity
+    final case object Failed extends Humidity
+  }
+
+}

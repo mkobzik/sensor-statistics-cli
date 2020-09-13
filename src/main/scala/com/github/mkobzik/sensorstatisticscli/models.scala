592863ecea3962bd7c900d75d21c2a192a4ef20d
Michał Kobzik <michal.kobzik@gmail.com>
2020-09-13 23:19:21 +0200

    Add Show instances for models

--- src/main/scala/com/github/mkobzik/sensorstatisticscli/models.scala
+++ src/main/scala/com/github/mkobzik/sensorstatisticscli/models.scala
@@ -1,5 +1,7 @@
 package com.github.mkobzik.sensorstatisticscli
 
+import cats.Show
+import cats.implicits.showInterpolator
 import com.github.mkobzik.sensorstatisticscli.models.Sensor.Id
 import io.estatico.newtype.macros.newtype
 
@@ -9,6 +11,13 @@ object models {
 
   object Sensor {
     @newtype final case class Id(value: Int)
+
+    object Id {
+      implicit val showId: Show[Id] = Show.show(id => s"Sensor.Id($id)")
+    }
+
+    implicit val showSensor: Show[Sensor] = Show.show(sensor => show"Sensor(${sensor.id})")
+
   }
 
   sealed trait Humidity
@@ -16,8 +25,18 @@ object models {
   object Humidity {
     final case class Measured(value: Int) extends Humidity
     final case object Failed extends Humidity
+
+    implicit val showHumidity: Show[Humidity] = Show.show[Humidity] {
+      case Measured(value) => s"Humidity.Measured($value)"
+      case Failed          => "Humidity.Failed"
+    }
+
   }
 
   final case class Sample(sensor: Sensor, humidity: Humidity)
+
+  object Sample {
+    implicit val showSample: Show[Sample] = Show.show(sample => show"Sample(${sample.sensor}, ${sample.humidity})")
+  }
 
 }

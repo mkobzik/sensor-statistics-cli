16ba5ca3b99ebaf02c1ccf89b5f71351bd83f58c
Michał Kobzik <michal.kobzik@gmail.com>
2020-09-18 02:43:04 +0200

    Show errors

--- src/main/scala/com/github/mkobzik/sensorstatisticscli/App.scala
+++ src/main/scala/com/github/mkobzik/sensorstatisticscli/App.scala
@@ -2,13 +2,12 @@ package com.github.mkobzik.sensorstatisticscli
 
 import java.nio.file.Path
 
-import cats.Monad
+import buildinfo.BuildInfo
 import cats.effect.Console.implicits._
-import cats.effect.{Blocker, Console, ExitCode, IO}
+import cats.effect.{Blocker, Console, ExitCode, IO, Sync}
 import cats.syntax.all._
 import com.monovore.decline._
 import com.monovore.decline.effect._
-import buildinfo.BuildInfo
 
 object App
     extends CommandIOApp(name = BuildInfo.name, header = "Calculate statistics from humidity sensor data", version = BuildInfo.version) {
@@ -28,10 +27,13 @@ object App
       .use(_ => IO.unit)
       .as(ExitCode.Success)
 
-  private def program[F[_]: SensorStatistics: Console: Monad](path: Path): F[Unit] = for {
+  private def program[F[_]: SensorStatistics: Console: Sync](path: Path): F[Unit] = (for {
     _     <- Console[F].putStrLn("Calculating statistics...")
     stats <- SensorStatistics[F].calculate(path)
     _     <- Console[F].putStrLn(stats)
-  } yield ()
+  } yield ()).handleErrorWith {
+    case err: errors.Error => Console[F].putStrLn(err)
+    case err               => Console[F].putStrLn(s"\u001b[31mUnknown error occurred! err = ${err.getMessage}\u001b[0m")
+  }
 
 }
--- src/main/scala/com/github/mkobzik/sensorstatisticscli/errors.scala
+++ src/main/scala/com/github/mkobzik/sensorstatisticscli/errors.scala
@@ -2,6 +2,7 @@ package com.github.mkobzik.sensorstatisticscli
 
 import java.nio.file.Path
 
+import cats.Show
 import com.github.mkobzik.sensorstatisticscli.models.Humidity
 
 object errors {
@@ -12,6 +13,15 @@ object errors {
     final case class CorruptedCsv(line: String) extends Error
     final case class HumidityOutOfRange(humidity: Humidity.Measured) extends Error
     final case class NotADirectory(path: Path) extends Error
+
+    implicit val errorShow: Show[Error] = Show.show(err =>
+      "\u001b[31m" + (err match {
+        case CorruptedCsv(line)           => s"Failed to parse cvs file. Corrupted line = '$line'"
+        case HumidityOutOfRange(humidity) => s"Wrong humidity value. Humidity should be in range [0, 100]. Value = ${humidity.value}"
+        case NotADirectory(path)          => s"Provided path does not point to directory. Path = '$path'"
+      }) + "\u001b[0m"
+    )
+
   }
 
 }
